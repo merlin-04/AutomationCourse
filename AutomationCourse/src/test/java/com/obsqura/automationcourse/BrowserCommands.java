17b62f624f57cffcd78f1a42ccb13aae1e9ab9b0
merlin-04 <merlinkuriakose9@gmail.com>
2023-08-21 22:07:42 +0530

    Removed unwanted files

---
+++ AutomationCourse/src/test/java/com/obsqura/automationcourse/BrowserCommands.java
@@ -0,0 +1,9 @@
+package com.obsqura.automationcourse;
+
+public class BrowserCommands extends Base {
+	public static void main(String args[]) {
+		BrowserCommands browsercommands = new BrowserCommands();
+		browsercommands.intializeBrowser();
+		browsercommands.driverCloseOrQuit();
+	}
+}
